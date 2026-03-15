101eaee89d4d780aedd3e4df27c8abff3f62c1ec
Audrey Lu <audreyylu04@gmail.com>
2026-03-15 12:29:38 -0700

    Added more error checking

--- src/main/java/UpdateSecurePassword.java
+++ src/main/java/UpdateSecurePassword.java
@@ -22,81 +22,82 @@ public class UpdateSecurePassword {
    */
   public static void main(String[] args) throws Exception {
 
-    String loginUser = "mytestuser";
-    String loginPasswd = "My6$Password";
-    String loginUrl = "jdbc:mysql://mysql-primary:3306/moviedb";
+    // allow overriding connection settings via environment variables or system properties
+    String loginUser = System.getenv().getOrDefault("DB_USER", System.getProperty("db.user", "mytestuser"));
+    String loginPasswd = System.getenv().getOrDefault("DB_PASS", System.getProperty("db.pass", "My6$Password"));
+    String dbHost = System.getenv().getOrDefault("DB_HOST", System.getProperty("db.host", "mysql-primary"));
+    String dbPort = System.getenv().getOrDefault("DB_PORT", System.getProperty("db.port", "3306"));
+    String dbName = System.getenv().getOrDefault("DB_NAME", System.getProperty("db.name", "moviedb"));
+
+    // Build JDBC URL. When running from outside the cluster, you can set DB_HOST to localhost after kubectl port-forward.
+    String loginUrl = String.format("jdbc:mysql://%s:%s/%s?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+        dbHost, dbPort, dbName);
+
+    System.out.println("Using JDBC URL: " + loginUrl);
+    System.out.println("DB user: " + loginUser);
 
     Class.forName("com.mysql.cj.jdbc.Driver");
-    Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
-    Statement statement = connection.createStatement();
 
-//        // change the employees table password column from VARCHAR(20) to VARCHAR(128)
-//        String alterEmployees = "ALTER TABLE employees MODIFY COLUMN password VARCHAR(128)";
-//        int alterEmpResult = statement.executeUpdate(alterEmployees);
-//        System.out.println("altering employees table schema completed, " + alterEmpResult + " rows affected");
-//
-//        // change the customers table password column from VARCHAR(20) to VARCHAR(128)
-//        String alterCustomers = "ALTER TABLE customers MODIFY COLUMN password VARCHAR(128)";
-//        int alterCustResult = statement.executeUpdate(alterCustomers);
-//        System.out.println("altering customers table schema completed, " + alterCustResult + " rows affected");
+    // Use try-with-resources so resources are always closed
+    try (Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+         Statement statement = connection.createStatement()) {
 
-    // we use the StrongPasswordEncryptor from jasypt library (Java Simplified Encryption)
-    //  it internally use SHA-256 algorithm and 10,000 iterations to calculate the encrypted password
-    PasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+        // we use the StrongPasswordEncryptor from jasypt library (Java Simplified Encryption)
+        //  it internally use SHA-256 algorithm and 10,000 iterations to calculate the encrypted password
+        PasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
 
-    // ---- Process customers table (primary key: id) ----
-    System.out.println("encrypting customers passwords (this might take a while)");
+        // ---- Process customers table (primary key: id) ----
+        System.out.println("encrypting customers passwords (this might take a while)");
 
-    String custQuery = "SELECT id, password FROM customers";
-    ResultSet rsCust = statement.executeQuery(custQuery);
+        String custQuery = "SELECT id, password FROM customers";
+        ResultSet rsCust = statement.executeQuery(custQuery);
 
-    PreparedStatement updateCustStmt = connection.prepareStatement("UPDATE customers SET password=? WHERE id=?");
+        PreparedStatement updateCustStmt = connection.prepareStatement("UPDATE customers SET password=? WHERE id=?");
 
-    int custCount = 0;
-    while (rsCust.next()) {
-      Object id = rsCust.getObject("id");
-      String password = rsCust.getString("password");
+        int custCount = 0;
+        while (rsCust.next()) {
+          Object id = rsCust.getObject("id");
+          String password = rsCust.getString("password");
 
-      String encryptedPassword = passwordEncryptor.encryptPassword(password);
+          String encryptedPassword = passwordEncryptor.encryptPassword(password);
 
-      updateCustStmt.setString(1, encryptedPassword);
-      // use setObject to handle numeric or string id types
-      updateCustStmt.setObject(2, id);
-      int updateResult = updateCustStmt.executeUpdate();
-      custCount += updateResult;
+          updateCustStmt.setString(1, encryptedPassword);
+          // use setObject to handle numeric or string id types
+          updateCustStmt.setObject(2, id);
+          int updateResult = updateCustStmt.executeUpdate();
+          custCount += updateResult;
+        }
+        rsCust.close();
+        updateCustStmt.close();
+
+        System.out.println("updating customers password completed, " + custCount + " rows affected");
+
+        // ---- Process employees table (primary key: email) ----
+        System.out.println("encrypting employees passwords (this might take a while)");
+
+        String empQuery = "SELECT email, password FROM employees";
+        ResultSet rsEmp = statement.executeQuery(empQuery);
+
+        PreparedStatement updateEmpStmt = connection.prepareStatement("UPDATE employees SET password=? WHERE email=?");
+
+        int empCount = 0;
+        while (rsEmp.next()) {
+          String email = rsEmp.getString("email");
+          String password = rsEmp.getString("password");
+
+          String encryptedPassword = passwordEncryptor.encryptPassword(password);
+
+          updateEmpStmt.setString(1, encryptedPassword);
+          updateEmpStmt.setString(2, email);
+          int updateResult = updateEmpStmt.executeUpdate();
+          empCount += updateResult;
+        }
+        rsEmp.close();
+        updateEmpStmt.close();
+
+        System.out.println("updating employees password completed, " + empCount + " rows affected");
+
     }
-    rsCust.close();
-    updateCustStmt.close();
-
-    System.out.println("updating customers password completed, " + custCount + " rows affected");
-
-    // ---- Process employees table (primary key: email) ----
-    System.out.println("encrypting employees passwords (this might take a while)");
-
-    String empQuery = "SELECT email, password FROM employees";
-    ResultSet rsEmp = statement.executeQuery(empQuery);
-
-    PreparedStatement updateEmpStmt = connection.prepareStatement("UPDATE employees SET password=? WHERE email=?");
-
-    int empCount = 0;
-    while (rsEmp.next()) {
-      String email = rsEmp.getString("email");
-      String password = rsEmp.getString("password");
-
-      String encryptedPassword = passwordEncryptor.encryptPassword(password);
-
-      updateEmpStmt.setString(1, encryptedPassword);
-      updateEmpStmt.setString(2, email);
-      int updateResult = updateEmpStmt.executeUpdate();
-      empCount += updateResult;
-    }
-    rsEmp.close();
-    updateEmpStmt.close();
-
-    System.out.println("updating employees password completed, " + empCount + " rows affected");
-
-    statement.close();
-    connection.close();
 
     System.out.println("finished");
 
--- src/main/java/VerifyPassword.java
+++ src/main/java/VerifyPassword.java
@@ -13,9 +13,9 @@ public class VerifyPassword {
 	 *   
 	 * Verify the password is simple:
 	 * success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
-	 * 
+	 *
 	 * Note that you need to use the same StrongPasswordEncryptor when encrypting the passwords
-	 * 
+	 *
 	 */
 	public static void main(String[] args) throws Exception {
 
@@ -26,34 +26,41 @@ public class VerifyPassword {
 
 	private static boolean verifyCredentials(String email, String password) throws Exception {
 		
-		String loginUser = "mytestuser";
-		String loginPasswd = "My6$Password";
-		String loginUrl = "jdbc:mysql://mysql-primary:3306/moviedb";
+		String loginUser = System.getenv().getOrDefault("DB_USER", System.getProperty("db.user", "mytestuser"));
+		String loginPasswd = System.getenv().getOrDefault("DB_PASS", System.getProperty("db.pass", "My6$Password"));
+		String dbHost = System.getenv().getOrDefault("DB_HOST", System.getProperty("db.host", "mysql-primary"));
+		String dbPort = System.getenv().getOrDefault("DB_PORT", System.getProperty("db.port", "3306"));
+		String dbName = System.getenv().getOrDefault("DB_NAME", System.getProperty("db.name", "moviedb"));
+
+		String loginUrl = String.format("jdbc:mysql://%s:%s/%s?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+				dbHost, dbPort, dbName);
+
+		System.out.println("Using JDBC URL: " + loginUrl);
 
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
-		Statement statement = connection.createStatement();
+		// try-with-resources
+		try (Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+			 Statement statement = connection.createStatement()) {
 
-		String query = String.format("SELECT * from customers where email='%s'", email);
+			String query = String.format("SELECT * from customers where email='%s'", email);
 
-		ResultSet rs = statement.executeQuery(query);
+			ResultSet rs = statement.executeQuery(query);
 
-		boolean success = false;
-		if (rs.next()) {
-		    // get the encrypted password from the database
-			String encryptedPassword = rs.getString("password");
-			
-			// use the same encryptor to compare the user input password with encrypted password stored in DB
-			success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
+			boolean success = false;
+			if (rs.next()) {
+			    // get the encrypted password from the database
+				String encryptedPassword = rs.getString("password");
+
+				// use the same encryptor to compare the user input password with encrypted password stored in DB
+				success = new StrongPasswordEncryptor().checkPassword(password, encryptedPassword);
+			}
+
+			rs.close();
+
+			System.out.println("verify " + email + " - " + password);
+
+			return success;
 		}
-
-		rs.close();
-		statement.close();
-		connection.close();
-		
-		System.out.println("verify " + email + " - " + password);
-
-		return success;
 	}
 
 }
