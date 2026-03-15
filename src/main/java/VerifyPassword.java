fdaeeb888ee3e42fed3735256213079a4222341a
Audrey Lu <audreyylu04@gmail.com>
2026-03-15 12:06:59 -0700

    Change localhost to mysql-primary to login in k8s cluster

--- src/main/java/VerifyPassword.java
+++ src/main/java/VerifyPassword.java
@@ -28,7 +28,7 @@ public class VerifyPassword {
 		
 		String loginUser = "mytestuser";
 		String loginPasswd = "My6$Password";
-		String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
+		String loginUrl = "jdbc:mysql://mysql-primary:3306/moviedb";
 
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
